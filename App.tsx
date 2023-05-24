ff3b3584a613bced6b578ba40aeb6e6911df9398
suosiyaz <siyazputhancode@gmail.com>
2023-05-24 17:55:56 +0530

    Assembling a Navigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
+import { createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
 });
+
+export default createAppContainer(navigator);
---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
